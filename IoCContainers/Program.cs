b9dc0c1610d2886828c9648a9e5aac053e599ae7
Joe <joelowrance@gmail.com>
2019-12-26 20:56:45 -0500

    Add Lamar and Microsoft containers

--- IoCContainers/Program.cs
+++ IoCContainers/Program.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices.ComTypes;
 using System.Security.Authentication.ExtendedProtection;
 using Autofac;
 using DependencyServices;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace IoCContainers
@@ -16,14 +17,49 @@ namespace IoCContainers
             //var result = instance.GetWeatherForLocation(21144);
             var demo = new AutoFacDemo();
             demo.Run();
+            
+            var d2 = new LamarDemo();
+            d2.Run();
+
+            var d3 = new MicrosoftDemo();
+            d3.Run();
+            
         }
     }
 
+    public class MicrosoftDemo
+    {
+        public void Run()
+        {
+            var provider = new Microsoft.Extensions.DependencyInjection.ServiceCollection()
+                .AddSingleton<IWeatherService, StubWeatherService>()
+                .AddSingleton<DemoService, DemoService>()
+                .BuildServiceProvider();
+
+            
+            var demoService =provider.GetRequiredService<DemoService>();
+            var temp = demoService.GetTemperature(21144);
+        }
+    }
+    
+    public class LamarDemo
+    {
+        public void Run()
+        {
+            var container = new Lamar.Container(x => { 
+                x.For<IWeatherService>().Use<StubWeatherService>();
+                x.For<DemoService>().Use<DemoService>();
+            });
+
+            var demo = container.GetInstance<DemoService>();
+            var temp = demo.GetTemperature(21144);
+        }
+    }
+    
     public class AutoFacDemo
     {
         public void Run()
         {
-            var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
             var containerBuilder = new Autofac.ContainerBuilder();
             containerBuilder.RegisterType<StubWeatherService>().As<IWeatherService>();
             containerBuilder.RegisterType<DemoService>().As<DemoService>();
